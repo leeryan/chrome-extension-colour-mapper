324ec08c3352f1085c57bab309c16f764fb0a2c8
Lee Ryan <squashedfrog26@me.com>
2013-09-24 22:02:01 +0100

    Increment or add logic implemented
    Still need to update correct part of the model

--- scripts/panel.js
+++ scripts/panel.js
@@ -1,23 +1,94 @@
 /* global Handlebars */
-var myApp = angular.module('myApp', []);
+var colourMapper = angular.module('colourMapper', []);
 
-var colourObj = {
-	property : 'colour',
+//Model returned from background script.
+var masterArr = [{
+	property : 'color',
 	declerations: [
-		{rule: 'rgb(51, 51, 51)', count: 47},
-		{rule: 'rgb(255, 255, 255)', count: 70},
-		{rule: 'rgb(59, 59, 59)', count: 0},
-		{rule: 'rgb(20, 20, 20)', count: 38},
-		{rule: 'rgb(66, 139, 202)', count: 26}
+		{rule: 'rgb(51, 51, 51)', count: 10},
+		{rule: 'rgb(255, 255, 255)', count: 10},
+		{rule: 'rgb(59, 59, 59)', count: 10},
+		{rule: 'rgb(20, 20, 20)', count: 10},
+		{rule: 'rgb(66, 139, 202)', count: 10}
+	]}, {
+	property : 'background',
+	declerations: [
+		{rule: 'rgb(51, 51, 51)', count: 10},
+		{rule: 'rgb(51, 51, 51)', count: 10},
+		{rule: 'rgb(97, 127, 16)', count: 10}
+	]}, {
+	property : 'border',
+	declerations: [
+		{rule: 'rgb(255, 255, 255)', count: 10},
+		{rule: 'rgb(0, 0, 0)', count: 10},
+		{rule: 'rgb(51, 51, 51)', count: 10},
+		{rule: 'rgb(189, 63, 66)', count: 10}
 	]
-};
+}];
 
-function ColourMapper($scope){
+
+//Controllers
+colourMapper.controller('mapperCtrl', ['$scope', function($scope){
+
+	var appPropertyObj = {};
 	
-	$scope.colourObj = colourObj;
-	$scope.declerations = colourObj.declerations;
-	$scope.colourTypes = ['RGB', 'HEX' ];
-	$scope.selected = $scope.colourTypes[0];
+	$scope.init = function(){
+	
+		//Create app model from master model
+		appPropertyObj = {
+			property : 'master',
+			properties : ['color', 'background', 'border'],
+			colourTypes: ['RGB', 'HEX' ],
+			declerations: [],
+			colours: []
+		};		
+	
+		//Setup variables to usin win controllers
+		$scope.appPropertyObj = appPropertyObj;
+		$scope.declerations = appPropertyObj.declerations;
+		$scope.colourTypes = appPropertyObj.colourTypes;
+		$scope.properties = appPropertyObj.properties;
+		$scope.selected = $scope.colourTypes[0];
+	
+		
+		for(var i = 0; i < masterArr.length; i++){
+			
+			_.map(masterArr[i].declerations, function(decleration){
+				
+				if(_.contains(appPropertyObj.colours, decleration.rule)){
+					console.log('increment');
+				} else {
+					console.log('add');
+					appPropertyObj.colours.push(decleration.rule);
+					appPropertyObj.declerations.push(decleration);
+				}
+				
+				
+				
+				
+			});
+			
+			_.each(appPropertyObj.declerations, function(decleration){
+				
+				
+				
+			})
+			
+		}
+		
+		console.log(appPropertyObj);
+		
+		// Find declerations in each array object. 
+		
+		//If the rule doesn;t exist add it.
+		
+		//Else just increment the couter.
+		
+		
+	
+	}
+
+	
 	
 	$scope.changeColourValue = function(element){
 		//Add active class to selected element
@@ -27,33 +98,32 @@ function ColourMapper($scope){
 			decleration.rule = $.rgbHex(decleration.rule);
 		});
 	};
-}
-
-CM.persistanceSwitcher = (function(){
+	
+	$scope.changePropertySet = function(property){
+		console.log('changePropertySet');
+		console.log(property);
 		
-		var bindPersistanceEvents = function(){
-		
-			var trigger = '.toggle label';
+		//Add property set to appPropertyObj
+		function addProperty(property){
 			
-			$(trigger).on('click', function(){
-				//ADD-REMOVE ON or OFF
-				//Update checkbox accordingly
-			});
-					
-		};
-	
-	return{
-		init: function(){
-			bindPersistanceEvents();
 		}
-	};
+		
+		//Remove property set to appPropertyObj
+		function removeProperty(property){
+			
+		}
+		
+		
+	}
 	
-})();
-*/
+}]);
 
-$(document).ready(function(){
-	//CM.createTables.init();
-	//CM.colourValueSwitcher.init();
-	//CM.persistanceSwitcher.init();
+//Directives
+colourMapper.directive('eatClick', function(){
+	return function(scope, element, attrs){
+		$(element).click(function(event){
+			event.preventDefault();
+		});
+	}
 });
 
